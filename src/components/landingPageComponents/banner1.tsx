f87ceb73d04931f92d298d4572eec4286762b988
Pedro Nevarez <peternevarez2@gmail.com>
2024-06-12 22:26:20 -0400

    updated copy on landing page for features

--- src/components/landingPageComponents/banner1.tsx
+++ src/components/landingPageComponents/banner1.tsx
@@ -10,23 +10,23 @@ export default function Banner1() {
         <div className=' flex justify-center items-center mobile:flex-col sm:flex-col '>
             <div className=' flex flex-col items-center'>
                 <BiBible className=' h-24 w-24'/>
-                <p className=' w-[400px] text-xs text-center text-gray-300 mt-2'>Deepen your connection with the Lord by engaging directly with the Holy Scriptures. Ask questions about faith, life, and spirituality, and receive guidance rooted in the wisdom of the Bible. Holy Harmony provides instant access to divine insights. Discover the transformative power of scripture and strengthen your relationship with the Lord, one conversation at a time</p>
+                <p className=' w-[400px] text-xs text-center text-gray-300 mt-2'>Personalized Biblical Guidance: Ask questions and receive insightful answers directly from the Holy Scriptures.</p>
             </div>
             <ShineBorder color={['#6B21A8']} className=' m-4 relative mobile:hidden'>
                 <div className=' flex flex-col items-center m-5 '>
                     <FaQuestionCircle className=' h-24 w-24'/>
-                    <p className=' w-[400px] text-sm text-center text-gray-300 mt-2'>Deepen your connection with the Lord by engaging directly with the Holy Scriptures. Ask questions about faith, life, and spirituality, and receive guidance rooted in the wisdom of the Bible. Holy Harmony provides instant access to divine insights. Discover the transformative power of scripture and strengthen your relationship with the Lord, one conversation at a time</p>
+                    <p className=' w-[400px] text-sm text-center text-gray-300 mt-2'>Daily Inspiration: Enjoy daily Bible readings and reflections to inspire your day.</p>
                 </div>
             </ShineBorder>
             <div className=' flex flex-col items-center m-5 sm:hidden md:hidden lg:hidden border-2 border-purple-700 rounded-2xl '>
                   <div className=' m-4 flex flex-col items-center '>
                     <FaQuestionCircle className=' h-24 w-24'/>
-                    <p className=' w-[400px] text-sm text-center text-gray-300 mt-2'>Deepen your connection with the Lord by engaging directly with the Holy Scriptures. Ask questions about faith, life, and spirituality, and receive guidance rooted in the wisdom of the Bible. Holy Harmony provides instant access to divine insights. Discover the transformative power of scripture and strengthen your relationship with the Lord, one conversation at a time</p>
+                    <p className=' w-[400px] text-sm text-center text-gray-300 mt-2'>Daily Inspiration: Enjoy daily Bible readings and reflections to inspire your day.</p>
                   </div>
             </div>
             <div className=' flex flex-col items-center'>
                 <FaPencil className=' h-24 w-24'/>
-                <p className=' w-[400px] text-xs text-center text-gray-300 mt-4'>Deepen your connection with the Lord by engaging directly with the Holy Scriptures. Ask questions about faith, life, and spirituality, and receive guidance rooted in the wisdom of the Bible. Holy Harmony provides instant access to divine insights. Discover the transformative power of scripture and strengthen your relationship with the Lord, one conversation at a time</p>
+                <p className=' w-[400px] text-xs text-center text-gray-300 mt-4'>Interactive Bible: Bring the Bible along with you anywhere you go. Highlight verses, take personal notes, and access them anytime, making your study more engaging and convenient.</p>
             </div>
         </div>
     </div>
